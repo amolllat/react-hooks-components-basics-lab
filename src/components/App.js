b6945c9955240b2bc4a558933ca17586162c048c
Amal Mohammed <157481527+amolllat@users.noreply.github.com>
2024-03-14 13:46:49 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,17 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+/* write an <About> component here */
+function About() {
+  return (
+    <div id = "about">
+      <h2>
+        About
+      </h2>
+    </div>
+  );
+}
+
 
 
 function App() {
@@ -24,7 +34,9 @@ function App() {
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home/>
       {/* add your <About> component here */}
+      <About/>
     </div>
   );
 }
